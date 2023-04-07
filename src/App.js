66b6d82ebe3afe25355e7e4f38668da04e35cee4
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-07 14:23:39 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
+import BookCreate from "./components/BookCreate";
 
 function App() {
+  const [books, setBooks] = useState([]);
   return (
     <div className="App">
       <h1>hello</h1>
+      <BookCreate />
     </div>
   );
 }
