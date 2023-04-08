792eeda0bea4bef22904c79429ad21b1e9754629
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-08 13:46:07 +0300

    add edit button and state

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BookShow = ({ book, onDelete }) => {
+  const [showEdit, setShowEdit] = useState(false);
+
   const handleDelete = () => {
     onDelete(book.id);
   };
 
+  console.log(showEdit);
+
   return (
     <div className="book-show">
-      <button type="submit" onClick={handleDelete}>
+      <button className="delete" type="submit" onClick={handleDelete}>
         X
+      </button>
+      <button className="edit" onClick={() => setShowEdit(!showEdit)}>
+        Edit
       </button>
       <h2>Book Title: {book.title}</h2>
       <h2>Book Author: {book.author}</h2>
