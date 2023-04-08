0de9832e21520d1a92742a733b261c4409a390d0
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-08 14:33:17 +0300

    fix edit form

--- src/components/BookEdit.js
+++ src/components/BookEdit.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const BookEdit = () => {
+  const [title, setTitle] = useState();
+  const [author, setAuthor] = useState();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("new title and author", title, author);
+  };
   return (
-    <form className="book-edit">
+    <form className="book-edit" onSubmit={handleSubmit}>
       <label>Title</label>
-      <input />
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
       <label>Author</label>
-      <input />
+      <input value={author} onChange={(e) => setAuthor(e.target.value)} />
       <button type="submit" className="button is-primary">
         Save
       </button>
