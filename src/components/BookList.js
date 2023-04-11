2f3a8f7dec65732741ff14a8183c49ab5046d06c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-11 17:14:39 +0300

    dislay the value of use Context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
+import BooksContext from "../context/books";
 import BookShow from "./BookShow";
 
 const BookList = ({ books, onDelete, onEdit }) => {
+  const value = useContext(BooksContext);
+
   return (
     <div className="book-list">
+      <h1>{value}</h1>
       {books.map((book) => {
         return (
           <BookShow
