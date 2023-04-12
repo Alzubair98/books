99058c60d45c4e4d440a5fb709aa0c55725c8ff1
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-12 15:54:34 +0300

    remove counter

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,14 +1,10 @@
 import React, { useContext } from "react";
-import BooksContext from "../context/books";
+
 import BookShow from "./BookShow";
 
 const BookList = ({ books, onDelete, onEdit }) => {
-  const { count, incrementCount } = useContext(BooksContext);
-
   return (
     <div className="book-list">
-      <h1>{count}</h1>
-      <button onClick={incrementCount}>click me</button>
       {books.map((book) => {
         return (
           <BookShow
--- src/context/books.js
+++ src/context/books.js
@@ -3,20 +3,7 @@ import { createContext, useState } from "react";
 const BooksContext = createContext();
 
 const Provider = ({ children }) => {
-  const [count, setCount] = useState(5);
-
-  const valueToShare = {
-    count,
-    incrementCount: () => {
-      setCount(count + 1);
-    },
-  };
-
-  return (
-    <BooksContext.Provider value={valueToShare}>
-      {children}
-    </BooksContext.Provider>
-  );
+  return <BooksContext.Provider value={6}>{children}</BooksContext.Provider>;
 };
 
 export { Provider };
