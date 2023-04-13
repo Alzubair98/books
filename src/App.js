621917201fc251d16094be296fa3cdcb8f26ec25
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2023-04-13 13:22:10 +0300

    add notice

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
 import useBooksContext from "./hooks/use_books_context";
+import "./app.css";
 // import { v4 as uuidv4 } from "uuid";
 
 function App() {
@@ -14,12 +15,10 @@ function App() {
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <span>
-        <strong>
-          The api im using dose not support edit function so the edit will not
-          be saved
-        </strong>
-      </span>
+      <div className="text-warning">
+        The api im using dose not support edit function so the edit will not be
+        saved
+      </div>
       <BookCreate />
       <BookList />
     </div>
